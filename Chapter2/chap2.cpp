58a8319d564d38e047e71cf06ac5e2d3e444c10e
umaiskhan42 <110044206+umaiskhan42@users.noreply.github.com>
2024-07-30 09:49:15 -0700

    fn

--- Chapter2/chap2.cpp
+++ Chapter2/chap2.cpp
@@ -10,3 +10,19 @@ int main(){
 
 	return 0;
 }
+
+
+#include<iostream>
+int doPrint(){
+	std::cout<< "Enter any number ";
+	int x{};
+	std::cin>>x;
+
+	return x;
+}
+int main(){
+	std::cout<<"Program Starts: "<<std::endl;
+	int y{doPrint()};
+	std::cout<<y;
+	return 0;
+}
