f01fc54069ba42cc3d3747ed9aa98759020a2ddb
umaiskhan42 <110044206+umaiskhan42@users.noreply.github.com>
2024-07-30 07:35:30 -0700

    cin

--- Chapter1/chapter1.cpp
+++ Chapter1/chapter1.cpp
@@ -60,3 +60,23 @@ int a;
 double b;
 
 */
+
+#include <iostream>
+
+int main()  
+{
+int a;         // no initializer (default initialization)
+int b = 5;     // initial value after equals sign (copy initialization)
+int c( 6 );    // initial value in parenthesis (direct initialization)
+
+// List initialization methods (C++11) (preferred)
+int d { 7 };   // initial value in braces (direct list initialization)
+int e = { 8 }; // initial value in braces after equals sign (copy list initialization)
+int f {};      // initializer is empty braces (value initialization)
+std::cout<<"Answers: 	"<<a<<b<<c<<d<<e<<f;  
+return 0;
+
+
+}
+
+//Prefer \n over std::endl
