4c56d0e53e300b06943ec6e127600637ab5c6922
umaiskhan42 <110044206+umaiskhan42@users.noreply.github.com>
2024-07-30 09:49:28 -0700

    fn args

--- Chapter2/chap2.cpp
+++ Chapter2/chap2.cpp
@@ -26,3 +26,17 @@ int main(){
 	std::cout<<y;
 	return 0;
 }
+
+
+// function arguments
+#include<iostream>
+
+int sumofnum(int x,int y){
+	std::cout<<"Sum of two numbers is : ";
+	return x+y;
+}
+int main(){
+	//sumofnum(2,3)
+	std::cout<<sumofnum(3,4);
+	return 0;
+}
