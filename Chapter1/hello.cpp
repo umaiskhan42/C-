2d894de47c5ee1330556cc92e33a35fa0f685c60
umaiskhan42 <110044206+umaiskhan42@users.noreply.github.com>
2024-07-30 04:54:36 -0700

    update chap1

--- Chapter1/hello.cpp
+++ Chapter1/hello.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
-#include <vector>
-#include <string>
+'''
+#include preprocessor directive indicates that we would like to use the 
+contents of the iostream library, 
+which is the part of the C++ standard library that allows us to 
+read and write text from/to the console.
+'''
 
-using namespace std;
-
-int main()
+int main() "Function whose name is main()"
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
 
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
+std::cout<<"Hello, World!";  "Charachter Outpur cout with << operator"
+return 0;
+'''
+This is a return statement. 
+When an executable program finishes running, 
+the program sends a value back to the operating system in order to indicate whether it ran successfully or not. 
+This particular return statement returns the value 0 to the operating system,
+ which means “everything went okay!”. 
+ This is the last statement in the program that executes.
+'''
 }
