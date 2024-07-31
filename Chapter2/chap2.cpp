085e5c230d8a8d544fdd29874cb0de8c76438931
Umais Khan <omaisk.me42ceme@student.nust.edu.pk>
2024-07-31 17:33:01 +0500

    testing github remote

--- Chapter2/chap2.cpp
+++ Chapter2/chap2.cpp
@@ -1,42 +1,50 @@
+// #include<iostream>
+
+// void doPrint(){
+
+// 	std::cout<<"This will print"<<std::endl;
+// }
+// int main(){
+
+// 	doPrint();
+
+// 	return 0;
+// }
+
+
+// #include<iostream>
+// int doPrint(){
+// 	std::cout<< "Enter any number ";
+// 	int x{};
+// 	std::cin>>x;
+
+// 	return x;
+// }
+// int main(){
+// 	std::cout<<"Program Starts: "<<std::endl;
+// 	int y{doPrint()};
+// 	std::cout<<y;
+// 	return 0;
+// }
+
+
+// // function arguments
+// #include<iostream>
+
+// int sumofnum(int x,int y){
+// 	std::cout<<"Sum of two numbers is : ";
+// 	return x+y;
+// }
+// int main(){
+// 	//sumofnum(2,3)
+// 	std::cout<<sumofnum(3,4);
+// 	return 0;
+// }
+
+
 #include<iostream>
 
-void doPrint(){
-
-	std::cout<<"This will print"<<std::endl;
-}
 int main(){
-
-	doPrint();
-
-	return 0;
-}
-
-
-#include<iostream>
-int doPrint(){
-	std::cout<< "Enter any number ";
-	int x{};
-	std::cin>>x;
-
-	return x;
-}
-int main(){
-	std::cout<<"Program Starts: "<<std::endl;
-	int y{doPrint()};
-	std::cout<<y;
-	return 0;
-}
-
-
-// function arguments
-#include<iostream>
-
-int sumofnum(int x,int y){
-	std::cout<<"Sum of two numbers is : ";
-	return x+y;
-}
-int main(){
-	//sumofnum(2,3)
-	std::cout<<sumofnum(3,4);
+	std::cout<<"Testing code "<<'\n'<<"Testing Code";
 	return 0;
 }
